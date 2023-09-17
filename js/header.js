07ac97222601bf8439192b05bdb16150ee782c82
GustavoPriftis <gustavopriftis727@gmail.com>
2023-09-17 14:30:58 -0300

    Adicionado background e navbar responsiva

--- js/header.js
+++ js/header.js
@@ -1,9 +1,16 @@
 const header =
-    `<div class="header">
-        <nav>
-            
+    `<header>
+        
+        <nav>            
             <a href="../index.html"><img src="../assets/rede.png" class="logo"></a>
-            <ul>
+            <div class="mobile-menu">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+                <div class="line4"></div>
+                <div class="line5"></div>
+            </div>
+            <ul class="nav-list">
                 <li><a href="../index.html">Exercício 1</a></li>
                 <li><a href="../pages/idade.html">Exercício 2</a></li>
                 <li><a href="../pages/mediaefrequencia.html">Exercício 3</a></li>
@@ -11,7 +18,8 @@ const header =
                 <li><a href="../pages/salario.html">Exercício 5</a></li>
             </ul>
         </nav>
-    </div>`
+     </header>
+     </div>`
 var a = document.createElement("header")
 document.body.appendChild(a)
 a.innerHTML = header
